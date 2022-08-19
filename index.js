5c185873250993d48503f5f1f92279780272d1aa
Calvin <ncalvin67@gmail.com>
2022-08-19 18:59:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase() === attribute.toLowerCase()
+    })
+  }
+  
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { 
+        return driver.toUpperCase().substring(0, attribute.length) === attribute.toUpperCase() 
+    })
+  }
+  
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { 
+        return driver.name === argument 
+    })
+  }
